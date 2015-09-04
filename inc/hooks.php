3ed17448362c9f46e1e88bc117c3285aed529e26
Alhadis <gardnerjohng@gmail.com>
2015-09-04 23:53:56 +1000

    Queue admin-specific DOM juice without delegation
    Unsure why stylesheets were being queued from a hook, but it was causing
    them to be loaded in the footer instead of the header. This had resulted
    in a brief flash-of-unstyled-content before the page had loaded.

--- inc/hooks.php
+++ inc/hooks.php
@@ -5,13 +5,8 @@ require_once 'hooks.settings.php';
 require_once 'hooks.shortcodes.php';
 
 
-/*
-#	Admin DOM juice
-add_action('admin_enqueue_scripts', function(){
-	wp_enqueue_script('admin', THEME_DIR . '/src/js/admin.js');
-});
-
-add_action('admin_head', function(){
-	wp_enqueue_style('admin', THEME_DIR . '/src/css/admin.css');
-});
-*/
+# Admin-only hooks
+if(WP_ADMIN){
+	# wp_enqueue_script('admin', THEME_DIR . '/src/js/admin.js');
+	# wp_enqueue_style('admin', THEME_DIR . '/src/css/admin.css');
+}
